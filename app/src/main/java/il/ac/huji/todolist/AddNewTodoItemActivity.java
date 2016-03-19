d92aedc1998b49ad4568d005c929dfea5457e010
shlomib2016 <shlomi.brielle@mail.huji.ac.il>
2016-03-19 15:12:07 +0200

    Assigment3

---
+++ app/src/main/java/il/ac/huji/todolist/AddNewTodoItemActivity.java
@@ -0,0 +1,13 @@
+package il.ac.huji.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewTodoItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_todo_item);
+    }
+}
---
+++ app/src/main/java/il/ac/huji/todolist/Model.java
@@ -0,0 +1,7 @@
+package il.ac.huji.todolist;
+
+/**
+ * Created by EstherShlomi on 3/18/2016.
+ */
+public class Model {
+}
